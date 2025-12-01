66ebd9bc479321e527c57f7aef21f3159dd22e78
Marlene <marlene.goedecke@ui.city>
2025-12-01 16:16:32 +0100

    add data base relations and foreign keys

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schema";
 
-const isTest = process.env.test;
-
-export const db = drizzle(process.env.DB_FILE_NAME!, { schema });
+export const db = drizzle(process.env.DB_FILE_NAME!, {
+  schema,
+  casing: "snake_case",
+});
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,49 +1,66 @@
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("users", {
   id: text().primaryKey(),
   email: text().notNull().unique(),
   password: text().notNull(),
-  createdAt: text("created_at")
+  createdAt: text()
     .notNull()
     .default(sql`(current_timestamp)`),
-  updatedAt: text("updated_at")
+  updatedAt: text()
     .notNull()
     .default(sql`(current_timestamp)`),
 });
 
-export const counter = sqliteTable("counter", {
+export const userRelations = relations(users, ({ many }) => ({
+  projects: many(projects),
+}));
+
+export const projects = sqliteTable("projects", {
   id: integer().primaryKey({ autoIncrement: true }),
-  user_id: integer("projectId").notNull(),
+  user_id: text()
+    .notNull()
+    .references(() => users.id),
   count: integer().notNull().default(1),
   direction: text({ enum: ["up", "down"] })
     .notNull()
     .default("up"),
-  name: text("name").notNull(),
-  needleSize: text("needle_size"),
-  createdAt: text("created_at")
+  name: text().notNull(),
+  needleSize: text(),
+  createdAt: text()
     .notNull()
     .default(sql`(current_timestamp)`),
-  updatedAt: text("updated_at")
+  updatedAt: text()
     .notNull()
     .default(sql`(current_timestamp)`),
   status: text({ enum: ["wip", "finished"] })
     .notNull()
     .default("wip"),
 });
+
+export const projectRelations = relations(projects, ({ one }) => ({
+  reminder: one(reminder),
+  user: one(users, {
+    fields: [projects.user_id],
+    references: [users.id],
+  }),
+}));
+
 export const reminder = sqliteTable("reminder", {
   id: integer().primaryKey({ autoIncrement: true }),
-  counter_id: integer("projectId").notNull(),
+  counter_id: integer()
+    .notNull()
+    .references(() => projects.id),
   kind: text({ enum: ["shortRow", "increase", "decrease", "patternRepeat"] })
     .notNull()
     .default("shortRow"),
   count: integer().notNull().default(1),
   step: integer().notNull().default(1),
-  createdAt: text("created_at")
+  createdAt: text()
     .notNull()
     .default(sql`(current_timestamp)`),
-  updatedAt: text("updated_at")
+  updatedAt: text()
     .notNull()
     .default(sql`(current_timestamp)`),
   startRow: integer("start_row").notNull(),
