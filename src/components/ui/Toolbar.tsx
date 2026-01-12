0668d0f7158658f48333ad08e41579d2e7422202
Marlene <marlene.goedecke@ui.city>
2026-01-12 09:00:52 +0100

    use radix toolbar

--- src/components/ui/Toolbar.tsx
+++ src/components/ui/Toolbar.tsx
@@ -1,15 +1,14 @@
 "use client";
-
+import * as ToolbarPrimitive from "@radix-ui/react-toolbar";
 import { cn } from "@/lib/utils";
 
 export function Toolbar({
   className,
   children,
   ...props
-}: React.ComponentPropsWithoutRef<"div">) {
+}: React.ComponentPropsWithoutRef<typeof ToolbarPrimitive.Root>) {
   return (
-    <div
-      role="toolbar"
+    <ToolbarPrimitive.Root
       className={cn(
         "tems-center relative flex w-fit gap-2 rounded-full border border-border bg-popup p-1 text-popup-foreground",
         className,
@@ -17,6 +16,42 @@ export function Toolbar({
       {...props}
     >
       {children}
-    </div>
+    </ToolbarPrimitive.Root>
+  );
+}
+
+export function ToolbarToogleGroup({
+  className,
+  children,
+  ...props
+}: React.ComponentPropsWithoutRef<typeof ToolbarPrimitive.ToolbarToggleGroup>) {
+  return (
+    <ToolbarPrimitive.ToolbarToggleGroup className={cn(className)} {...props}>
+      {children}
+    </ToolbarPrimitive.ToolbarToggleGroup>
+  );
+}
+
+export function ToolbarToogleItem({
+  className,
+  children,
+  ...props
+}: React.ComponentPropsWithoutRef<typeof ToolbarPrimitive.ToolbarToggleItem>) {
+  return (
+    <ToolbarPrimitive.ToolbarToggleItem className={cn(className)} {...props}>
+      {children}
+    </ToolbarPrimitive.ToolbarToggleItem>
+  );
+}
+
+export function ToolbarButton({
+  className,
+  children,
+  ...props
+}: React.ComponentPropsWithoutRef<typeof ToolbarPrimitive.ToolbarButton>) {
+  return (
+    <ToolbarPrimitive.ToolbarButton className={cn(className)} {...props}>
+      {children}
+    </ToolbarPrimitive.ToolbarButton>
   );
 }
