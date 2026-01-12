d59144a5b8a5c070e72c8ac216b969707e62c1cc
Marlene <marlene.goedecke@ui.city>
2026-01-12 17:32:42 +0100

    create seperate context for count direction

---
+++ src/contexts/CountDirectionContext.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { createContext, use, useMemo, useState } from "react";
+import { type CountDirection } from "@/lib/types";
+
+export const CountDirectionContext = createContext<{
+  direction: CountDirection;
+  toggleDirection: () => void;
+} | null>(null);
+
+export function CountDirectionProvider({
+  children,
+  initialDirection,
+}: React.PropsWithChildren & { initialDirection: CountDirection }) {
+  const [direction, setDirection] = useState(initialDirection);
+
+  const contextValue = useMemo(() => {
+    const toggleDirection = () => {
+      if (direction === "up") {
+        setDirection("down");
+        return;
+      }
+      setDirection("up");
+    };
+    return { direction, toggleDirection };
+  }, [direction, setDirection]);
+
+  return (
+    <CountDirectionContext value={contextValue}>
+      {children}
+    </CountDirectionContext>
+  );
+}
+
+export function useCountDirection(): [CountDirection, () => void] {
+  const ctx = use(CountDirectionContext);
+  if (!ctx) throw new Error("Must be used within DirectionProvider");
+
+  const { direction, toggleDirection } = ctx;
+
+  return [direction, toggleDirection];
+}
