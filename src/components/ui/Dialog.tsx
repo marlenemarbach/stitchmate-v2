36d1d34fe73fe471163cd785618fbedc6e766163
Marlene <marlene.goedecke@ui.city>
2026-01-21 21:52:23 +0100

    replace dialog with radix ui component

--- src/components/ui/Dialog.tsx
+++ src/components/ui/Dialog.tsx
@@ -1,202 +1,122 @@
 "use client";
 
-import { createContext, useContext, useState } from "react";
+import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { X } from "lucide-react";
 import { AnimatePresence, motion } from "motion/react";
-import { createPortal } from "react-dom";
 import { cn } from "@/lib/utils";
-import { Button, ButtonProps } from "./Button";
-import { createSlot } from "./slot/slot";
 
-/* ---------------------------------------------------------------------------
- *  Dialog Root Element
- * -------------------------------------------------------------------------*/
-
-export type DialogProps = {
-  open?: boolean;
-  setOpen?: React.Dispatch<React.SetStateAction<boolean>>;
-};
-
-const DialogContext = createContext<Required<DialogProps> | null>(null);
+function DialogRoot({
+  children,
+  ...props
+}: React.ComponentPropsWithoutRef<typeof DialogPrimitive.Root>) {
+  return <DialogPrimitive.Root {...props}>{children}</DialogPrimitive.Root>;
+}
 
 export function Dialog({
-  children,
+  className,
   open,
-  setOpen,
-}: React.PropsWithChildren & DialogProps) {
-  const [internalOpen, setInternalOpen] = useState(false);
-
-  const isControlled = setOpen !== undefined && open !== undefined;
-
-  const contextValue = {
-    open: isControlled ? (open ?? false) : internalOpen,
-    setOpen: isControlled ? setOpen : setInternalOpen,
-  };
-
-  return (
-    <DialogContext.Provider value={contextValue}>
-      {children}
-    </DialogContext.Provider>
-  );
-}
-
-/* -----------------------------------------------------------------------
- * Dialog Content Container
- * ---------------------------------------------------------------------*/
-
-export type DialogContentProps = React.PropsWithChildren & {
-  className?: string;
-};
-
-export function DialogContent({
+  onOpenChange,
   children,
-  className,
-}: React.PropsWithChildren & { className?: string }) {
-  const ctx = useContext(DialogContext);
-
-  if (!ctx) throw new Error("<DialogContainer> must be used within <Dialog>");
-  const { open } = ctx;
-
+  ...props
+}: React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> & {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}) {
   // Only render portal on client-side to avoid SSR errors
-  if (typeof document === "undefined") {
-    return null;
-  }
-
-  return createPortal(
-    <AnimatePresence>
-      {open && (
-        <>
-          <DialogOverlay />
-          <motion.div
-            className={cn(
-              "fixed top-1/2 left-1/2 z-50 flex w-[calc(100vw_-_2rem)] -translate-1/2 flex-col gap-6 rounded-xl bg-popup p-6 text-popup-foreground shadow-popup sm:w-sm",
-              className,
-            )}
-            initial={{ opacity: 0, scale: 0.9 }}
-            animate={{
-              opacity: 1,
-              scale: 1,
-              transition: { ease: "easeOut", duration: 0.3 },
-            }}
-            exit={{
-              opacity: 0,
-              scale: 0.9,
-              transition: { ease: "easeIn", duration: 0.2 },
-            }}
-            key="dialogContent"
-          >
-            {children}
-          </motion.div>
-        </>
-      )}
-    </AnimatePresence>,
-    document.body,
-  );
-}
-
-/* --------------------------------------------------------------------------
- * Dialog Overlay
- * * ----------------------------------------------------------------------*/
-
-function DialogOverlay({
-  children,
-  className,
-}: React.PropsWithChildren & { className?: string }) {
-  const ctx = useContext(DialogContext);
-
-  if (!ctx) throw new Error("<DialogOverlay> must be used within <Dialog>");
-  const { setOpen } = ctx;
+  // if (typeof document === "undefined") {
+  //   return null;
+  // }
 
   return (
-    <motion.div
-      className={cn(
-        "fixed inset-0 z-50 h-screen w-screen bg-black/40 backdrop-blur-xs",
-        className,
-      )}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      key="modalOverlay"
-      onClick={() => setOpen(false)}
-    >
-      {children}
-    </motion.div>
+    <DialogRoot open={open} onOpenChange={onOpenChange}>
+      <AnimatePresence>
+        {open && (
+          <DialogPrimitive.Portal forceMount>
+            <DialogPrimitive.Overlay key={"dialogOverlay"} asChild>
+              <motion.div
+                className="fixed inset-0 isolate z-50 bg-black/10"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              />
+            </DialogPrimitive.Overlay>
+            <DialogPrimitive.Content {...props} asChild>
+              <motion.div
+                className={cn(
+                  "fixed top-1/2 left-1/2 z-50 grid w-[calc(100vw_-_2rem)] -translate-1/2 gap-6 rounded-xl bg-background p-6 shadow-popup sm:w-sm",
+                  className,
+                )}
+                initial={{ opacity: 0, scale: 0.9 }}
+                animate={{
+                  opacity: 1,
+                  scale: 1,
+                  transition: { ease: "easeOut", duration: 0.3 },
+                }}
+                exit={{
+                  opacity: 0,
+                  scale: 0.9,
+                  transition: { ease: "easeIn", duration: 0.2 },
+                }}
+                key="dialogContent"
+              >
+                {children}
+              </motion.div>
+            </DialogPrimitive.Content>
+          </DialogPrimitive.Portal>
+        )}
+      </AnimatePresence>
+    </DialogRoot>
   );
 }
-
-/* ---------------------------------------------------------------------------
- * Dialog Trigger
- * -------------------------------------------------------------------------*/
-
-export type DialogTriggerProps = ButtonProps & { asChild?: boolean };
 
 export function DialogTrigger({
-  variant,
-  size,
-  asChild,
   children,
-  className,
   ...props
-}: DialogTriggerProps) {
-  const ctx = useContext(DialogContext);
-
-  if (!ctx) throw new Error("<DialogTrigger> must be used within <Dialog>");
-  const { setOpen } = ctx;
-
-  const triggerProps = {
-    onClick: () => setOpen(true),
-    ...props,
-  };
-
-  if (asChild) {
-    return createSlot(children, triggerProps);
-  }
-
+}: React.ComponentPropsWithoutRef<typeof DialogPrimitive.Trigger>) {
   return (
-    <Button
-      className={cn("w-fit", className)}
-      variant={variant}
-      size={size}
-      {...triggerProps}
-    >
+    <DialogPrimitive.Trigger {...props} asChild>
       {children}
-    </Button>
+    </DialogPrimitive.Trigger>
   );
 }
-
-/* ---------------------------------------------------------------------------
- *  Dialog Title
- * -------------------------------------------------------------------------*/
 
 export function DialogTitle({
   children,
   className,
   ...props
-}: React.ComponentPropsWithoutRef<"h2">) {
+}: React.ComponentPropsWithoutRef<typeof DialogPrimitive.Title>) {
   return (
-    <h2 className={cn("text-lg", className)} {...props}>
+    <DialogPrimitive.Title className={cn("text-lg", className)} {...props}>
       {children}
-    </h2>
+    </DialogPrimitive.Title>
   );
 }
 
-/* ---------------------------------------------------------------------------
- *  Dialog Close
- * -------------------------------------------------------------------------*/
-
-export function DialogClose() {
-  const ctx = useContext(DialogContext);
-
-  if (!ctx) throw new Error("<DialogClose> must be used within <Dialog>");
-  const { setOpen } = ctx;
-
+export function DialogClose({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<typeof DialogPrimitive.Close>) {
   return (
-    <Button
-      variant="ghost"
+    <DialogPrimitive.Close
       className="150ms absolute top-4 right-4 h-6 w-6 cursor-pointer p-0 text-muted-foreground transition-colors ease-out"
-      onClick={() => setOpen(false)}
+      {...props}
     >
       <X className="size-4" />
-    </Button>
+    </DialogPrimitive.Close>
+  );
+}
+
+export function DialogDescription({
+  children,
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<typeof DialogPrimitive.Description>) {
+  return (
+    <DialogPrimitive.Description
+      className={cn("sr-only", className)}
+      {...props}
+    >
+      {children}
+    </DialogPrimitive.Description>
   );
 }
