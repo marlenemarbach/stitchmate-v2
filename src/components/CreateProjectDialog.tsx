3fb80a9057d88b52164805691d02c901f2c638bd
Marlene <marlene.goedecke@ui.city>
2026-01-22 10:46:50 +0100

    use radix dialog and adjust styles

--- src/components/CreateProjectDialog.tsx
+++ src/components/CreateProjectDialog.tsx
@@ -2,7 +2,7 @@
 
 import { useActionState, useState } from "react";
 import { useRouter } from "next/navigation";
-import { ArrowRight } from "lucide-react";
+import { Plus } from "lucide-react";
 import { ProjectData, createProject } from "../actions/projects";
 import { ActionResponse } from "../actions/types";
 import { Button } from "./ui/Button";
@@ -10,23 +10,24 @@ import {
   Dialog,
   DialogClose,
   DialogContent,
+  DialogFooter,
   DialogTitle,
   DialogTrigger,
 } from "./ui/Dialog";
 import { Form, FormError, FormField } from "./ui/Form";
-import { Check } from "./ui/Icons";
 import { Input } from "./ui/Input";
-import { Label } from "./ui/Label";
 
 const initialState: ActionResponse = {
   success: false,
-  message: "someting bad happened",
+  message: "",
   error: undefined,
 };
 
-export function CreateProjectDialog({ children }: React.PropsWithChildren) {
+export function CreateProjectDialog() {
   const router = useRouter();
-  const [isOpen, setIsOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const autoFocus = (el: HTMLInputElement) => el?.focus();
 
   const [state, formAction, pending] = useActionState<ActionResponse, FormData>(
     async (prev: ActionResponse, formData: FormData) => {
@@ -38,8 +39,8 @@ export function CreateProjectDialog({ children }: React.PropsWithChildren) {
         const result = await createProject(data);
 
         if (result.success) {
-          setIsOpen(false);
-          router.push(`/projects/${result.projectId}`);
+          setOpen(false);
+          router.push(`/projects/${result.project?.id}`);
         }
         return result;
       } catch (error) {
@@ -55,43 +56,56 @@ export function CreateProjectDialog({ children }: React.PropsWithChildren) {
   );
 
   return (
-    <Dialog open={isOpen} setOpen={setIsOpen}>
-      {children}
-      <DialogContent className="pb-8">
+    <Dialog open={open} onOpenChange={setOpen} modal>
+      <DialogTrigger>
+        <Button
+          size="icon"
+          className="ml-auto bg-indigo-400/90 text-foreground hover:bg-indigo-400"
+          onClick={() => setOpen(true)}
+        >
+          <span className="sr-only">New Project</span>
+          <Plus className="stroke-3" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent open={open} className="top-[28%] sm:top-[30%] sm:w-lg">
         <DialogClose />
-        <DialogTitle>Create a new project</DialogTitle>
+        <DialogTitle>New Project:</DialogTitle>
         <Form action={formAction} autoComplete="off" className="gap-3">
           <FormField>
-            <Label htmlFor="title">Title:</Label>
-            <div className="flex gap-2">
-              <Input
-                id="title"
-                name="title"
-                type="text"
-                placeholder="Enter a title"
-                disabled={pending}
-                aria-describedby="title-error"
-                max={30}
-                required
-              />
-              <Button className="rounded-lg">
-                <ArrowRight className="size-5" />
-              </Button>
-            </div>
+            <label htmlFor="title" className="sr-only">
+              Title:
+            </label>
+            <Input
+              className="border-none pl-0 text-lg font-medium focus-visible:ring-transparent focus-visible:outline-none"
+              id="title"
+              name="title"
+              type="text"
+              placeholder="Enter a title"
+              disabled={pending}
+              aria-describedby="title-error"
+              max={30}
+              required
+              ref={autoFocus}
+            />
+            {(state.error || state?.errors) && (
+              <FormError id="name-error">
+                {state?.errors?.name ?? state.message}
+              </FormError>
+            )}
           </FormField>
-          {(state.error || state?.errors) && (
-            <FormError id="name-error">
-              {state?.errors?.name ?? state.message}
-            </FormError>
-          )}
+          <DialogFooter>
+            <Button
+              className="col-span-3 col-start-6 w-fit place-self-end"
+              variant="secondary"
+            >
+              cancel
+            </Button>
+            <Button className="w-fit place-self-end" type="submit">
+              save
+            </Button>
+          </DialogFooter>
         </Form>
       </DialogContent>
     </Dialog>
   );
-}
-
-export function CreateProjectDialogTrigger({
-  children,
-}: React.PropsWithChildren) {
-  return <DialogTrigger asChild>{children}</DialogTrigger>;
 }
