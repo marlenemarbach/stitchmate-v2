9c3cfb951d2dacd9d2d6a5e77e6f3853e3a0b60a
Marlene <marlene.goedecke@ui.city>
2026-01-23 17:51:04 +0100

    adjust footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,25 +1,42 @@
 "use server";
-import { Copyright, Mail } from "lucide-react";
+import Link from "next/link";
+import { Copyright } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 export async function Footer() {
   return (
     <footer
-      className={cn("flex items-center justify-between p-5 text-sm opacity-50")}
+      className={cn(
+        "flex justify-between gap-2 border-t border-border/50 px-5 pt-5 pb-6 text-xs",
+      )}
     >
-      <a
-        className="w-fit cursor-pointer rounded-lg p-1 opacity-50 transition-[ring,opacity] duration-250 ease-out outline-none hover:opacity-70 focus-visible:opacity-70 focus-visible:ring-[1px] focus-visible:ring-ring"
-        href="https://www.instagram.com/crafty_stitchess/"
-        target="_blank"
-      >
-        created by @craftystitchess
-      </a>
-      <a
-        className="cursor-pointer rounded-lg p-1 opacity-50 transition-[ring,opacity] duration-250 ease-out outline-none hover:opacity-70 focus-visible:opacity-70 focus-visible:ring-[1px] focus-visible:ring-ring"
-        href="mailto:stitchmate.contact@gmail.com?subject=Hello"
-      >
-        <Mail className="size-5" />
-      </a>
+      <div className="flex gap-1">
+        <span className="flex items-center gap-1 opacity-50">
+          <Copyright className="size-3" />
+          2026
+        </span>
+        <a
+          className="opacity-50 transition-opacity duration-150 ease-out outline-none hover:opacity-70 focus-visible:opacity-70"
+          href="https://www.instagram.com/crafty_stitchess/"
+          target="_blank"
+        >
+          by @craftystitchess
+        </a>
+      </div>
+      <div className="flex items-center justify-center gap-6">
+        <a
+          className="opacity-50 transition-opacity duration-150 ease-out outline-none hover:opacity-70 focus-visible:opacity-70"
+          href="mailto:stitchmate.contact@gmail.com?subject=Hello"
+        >
+          Contact
+        </a>
+        <Link
+          className="opacity-50 transition-opacity duration-150 ease-out outline-none hover:opacity-70 focus-visible:opacity-70"
+          href="/privacy"
+        >
+          Privacy
+        </Link>
+      </div>
     </footer>
   );
 }
