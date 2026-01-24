9d89bb92731843c13e774fb46273ca2c74856a53
Marlene <marlene.goedecke@ui.city>
2026-01-24 11:06:05 +0100

    remove getProject action

--- src/actions/projects.ts
+++ src/actions/projects.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { refresh, revalidatePath } from "next/cache";
+import { revalidatePath } from "next/cache";
 import { notFound, redirect } from "next/navigation";
 import z from "zod";
 import {
