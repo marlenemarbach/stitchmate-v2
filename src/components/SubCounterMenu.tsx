6da7a92d3783f73d097e104504d22c6438f9478f
Marlene <marlene.goedecke@ui.city>
2026-01-24 18:56:58 +0100

    implement subCounter formAction

--- src/components/SubCounterMenu.tsx
+++ src/components/SubCounterMenu.tsx
@@ -1,19 +1,12 @@
 "use client";
 
-import { startTransition } from "react";
-import {
-  ArrowUpRight,
-  ChevronDown,
-  ChevronUp,
-  IterationCw,
-} from "lucide-react";
-import { toast } from "sonner";
-import { useSubCounter } from "@/contexts/SubCounterContext";
-import { SubCounter } from "@/lib/types";
+import { useActionState, useState } from "react";
+import { ChevronDown, ChevronUp, IterationCw } from "lucide-react";
+import { ActionResponse } from "@/actions/types";
 import { type ProjectWithSubCounter } from "@/lib/types";
 import { updateSubCounter } from "../actions/subCounter";
 import { Button } from "./ui/Button";
-import { Label } from "./ui/Label";
+import { FormError } from "./ui/Form";
 import {
   NumberSpinner,
   NumberSpinnerButton,
@@ -22,108 +15,111 @@ import {
 import { Switch } from "./ui/Switch";
 
 // TODO:
-// - provide visual feedback on the subcounter errors and pending state
-// - debounce number input
+// - provide visual feedback for pending state
+
+const initalState: ActionResponse = {
+  success: false,
+  message: "",
+  error: undefined,
+};
 
 export function SubCounterMenu({
   project,
 }: {
   project: ProjectWithSubCounter;
 }) {
-  const { setState } = useSubCounter();
+  //  forces the form to rerender via key change, to keep input states internal
+  const [formKey, setFormKey] = useState(1);
 
-  function handleSubCounterChange(data: Partial<SubCounter>) {
-    try {
-      startTransition(async () => {
-        await updateSubCounter(project.id, data);
-      });
-      if (data.active != undefined) setState("on");
-    } catch (error) {
-      console.error("Update subcounter error:", error);
-      toast.error("Subcounter could not be updated");
-    }
-  }
+  const [state, formAction, pending] = useActionState<ActionResponse, FormData>(
+    async (prev: ActionResponse, formData: FormData) => {
+      try {
+        const data = {
+          startRow: Number(formData.get("startRow")),
+          interval: Number(formData.get("interval")),
+          active: formData.get("active") === "on",
+        };
+        console.log("data", data, formData.get("startRow"));
+        const result = await updateSubCounter(project.id, data);
+        return result;
+      } catch (e) {
+        console.error("Update subcounter error:", e);
+        return {
+          success: false,
+          message: "An error occured while updating your subcounter",
+          error: "Failed to update subcounter",
+        };
+      }
+    },
+    initalState,
+  );
 
   return (
-    <div className="grid gap-6">
-      <h2 className="mt-1 text-center text-lg font-medium">Subcounter</h2>
-      <form className="grid grid-cols-2 grid-rows-3 gap-y-1">
-        <p className="col-span-2 mt-1 ml-2 h-fit self-start text-sm text-muted-foreground">
-          Select start row and row interval:
-        </p>
-        <div className="col-span-2 flex items-center gap-2 rounded-lg bg-primary/5 pr-4 pl-6">
-          <label htmlFor="startRow">from:</label>
-          <NumberSpinner
-            min={1}
-            max={99}
-            defaultValue={5}
-            className="bg-transparent"
-          >
-            <NumberSpinnerInput id={"startRow"} />
-            <div className="grid">
-              <NumberSpinnerButton direction={1} title="Increment start row">
-                <ChevronUp strokeWidth={3} />
-              </NumberSpinnerButton>
-              <NumberSpinnerButton direction={-1} title="Decrement start row">
-                <ChevronDown strokeWidth={3} />
-              </NumberSpinnerButton>
-            </div>
-          </NumberSpinner>
+    <form className="grid gap-6" key={formKey} action={formAction}>
+      <div className="mb-2 flex items-baseline justify-between">
+        <h2 className="text-lg font-medium">Subcounter</h2>
+        <Switch
+          aria-label="Subcounter notification"
+          id="active"
+          name="active"
+          defaultChecked={project.subCounter.active}
+        />
+      </div>
+      <div className="grid grid-cols-2 gap-x-6 gap-y-3">
+        <label className="text-sm" htmlFor="startRow">
+          Start row:
+        </label>
 
-          <label className="ml-auto" htmlFor="interval">
-            every:
-          </label>
-          <NumberSpinner min={1} max={99} defaultValue={5}>
-            <NumberSpinnerInput id={"interval"} />
-            <div className="grid">
-              <NumberSpinnerButton direction={1} title="Increment row interval">
-                <ChevronUp strokeWidth={3} />
-              </NumberSpinnerButton>
-              <NumberSpinnerButton
-                direction={-1}
-                title="Decrement row interval"
-              >
-                <ChevronDown strokeWidth={3} />
-              </NumberSpinnerButton>
-            </div>
-          </NumberSpinner>
-        </div>
+        <label className="text-sm" htmlFor="interval">
+          Row interval:
+        </label>
+
+        <NumberSpinner min={1} max={99} defaultValue={project.count}>
+          <NumberSpinnerInput id="startRow" name="startRow" />
+          <div className="grid h-full py-1">
+            <NumberSpinnerButton direction={1} title="Increment start row">
+              <ChevronUp strokeWidth={3} />
+            </NumberSpinnerButton>
+            <NumberSpinnerButton direction={-1} title="Decrement start row">
+              <ChevronDown strokeWidth={3} />
+            </NumberSpinnerButton>
+          </div>
+        </NumberSpinner>
+
+        <NumberSpinner min={1} max={99} defaultValue={1}>
+          <NumberSpinnerInput id="interval" name="interval" />
+          <div className="grid h-full py-1">
+            <NumberSpinnerButton direction={1} title="Increment row interval">
+              <ChevronUp strokeWidth={3} />
+            </NumberSpinnerButton>
+            <NumberSpinnerButton direction={-1} title="Decrement row interval">
+              <ChevronDown strokeWidth={3} />
+            </NumberSpinnerButton>
+          </div>
+        </NumberSpinner>
+
         <Button
-          variant="ghost"
           type="button"
           size="small"
-          className="w-fit self-center rounded-lg p-1 pr-2 text-muted-foreground"
+          variant="ghost"
+          className="mt-2 w-fit p-0 text-muted-foreground hover:bg-background hover:text-foreground/80 focus-visible:text-foreground/80 focus-visible:ring-transparent focus-visible:outline-none"
+          onClick={() => setFormKey((prev) => prev + 1)}
         >
           <IterationCw className="size-4" />
           reset
         </Button>
 
         <Button
-          type="button"
+          disabled={pending}
+          type="submit"
           size="small"
           variant="ghost"
-          className="self-center justify-self-end rounded-lg pr-2 pl-1"
+          className="mt-2 justify-self-end p-0 text-blue-400/85 hover:bg-background hover:text-blue-400 focus-visible:text-blue-400 focus-visible:ring-transparent focus-visible:outline-none"
         >
-          <ArrowUpRight className="size-4" />
-          save
+          save changes
         </Button>
-      </form>
-      <Label
-        htmlFor="notification"
-        className="flex w-full items-center justify-end gap-3 text-base"
-      >
-        <div className="flex items-center gap-2">
-          <span>Show Subcounter</span>
-        </div>
-        <Switch
-          id="notification"
-          name="notification"
-          defaultChecked={project.subCounter.active}
-          onCheckedChange={(isChecked) =>
-            handleSubCounterChange({ active: isChecked })
-          }
-        ></Switch>
-      </Label>
-    </div>
+      </div>
+      {state.errors && <FormError>{state.message}</FormError>}
+    </form>
   );
 }
