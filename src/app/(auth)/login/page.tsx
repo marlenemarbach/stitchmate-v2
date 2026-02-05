70e772cb6cc99f07c700f06ec7ba5154f3ce0606
Marlene <marlene.goedecke@ui.city>
2026-02-05 17:42:59 +0100

    remove mockDelay

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,5 @@
 import { LoginForm } from "@/components/LoginForm";
-import { mockDelay } from "@/lib/utils";
 
 export default async function Login() {
-  await mockDelay(1000);
   return <LoginForm />;
 }
