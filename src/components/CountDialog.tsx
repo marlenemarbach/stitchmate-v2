0005d092756e719e2f5f110e669415f415e0d417
Marlene <marlene.goedecke@ui.city>
2026-02-22 08:18:27 +0100

    sync with count state

--- src/components/CountDialog.tsx
+++ src/components/CountDialog.tsx
@@ -1,7 +1,6 @@
-import { useActionState, useEffect, useState } from "react";
-import { IterationCw } from "lucide-react";
+import { useEffect, useState, useTransition } from "react";
+import { IterationCw, Loader } from "lucide-react";
 import { updateProject } from "@/actions/projects";
-import { ActionResponse } from "@/actions/types";
 import { useCount } from "@/contexts/CountContext";
 import { type ProjectWithSubCounter } from "@/lib/types";
 import { Button } from "./ui/Button";
@@ -14,13 +13,15 @@ import {
   DialogTitle,
 } from "./ui/Dialog";
 import { FormError } from "./ui/Form";
-import { NumberSpinner } from "./ui/NumberSpinner";
-
-const initalState: ActionResponse = {
-  success: false,
-  message: "",
-  error: undefined,
-};
+import {
+  SpinButton,
+  SpinButtonDecrement,
+  SpinButtonError,
+  SpinButtonField,
+  SpinButtonGroup,
+  SpinButtonIncrement,
+  SpinButtonLabel,
+} from "./ui/SpinButton";
 
 export function CountDialog({
   project,
@@ -32,92 +33,95 @@ export function CountDialog({
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
   const { count } = useCount();
-
-  // temporary solution, force the form to rerender to control input value
-  const [formValue, setFormValue] = useState(count);
-
-  const [state, formAction, pending] = useActionState<ActionResponse, FormData>(
-    async (_, formData: FormData) => {
-      try {
-        const data = {
-          count: Number(formData.get("count")),
-        };
-
-        const result = await updateProject(data, project.id);
-        if (result.success) setOpen(false);
-
-        return result;
-      } catch (e) {
-        console.error("Update project error:", e);
-        return {
-          success: false,
-          message: "An error occured while updating your project",
-          error: "Failed to update project",
-        };
-      }
-    },
-    initalState,
-  );
+  const [value, setValue] = useState(count.toString());
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    setFormValue(count);
+    setValue(count.toString());
   }, [count]);
+
+  const [pending, startTransition] = useTransition();
+
+  function submitProjectCount() {
+    startTransition(async () => {
+      try {
+        const result = await updateProject(
+          { count: parseInt(value) },
+          project.id,
+        );
+        if (result.success) {
+          setOpen(false);
+        } else {
+          setMessage("An error occured while saving your row count");
+        }
+      } catch (error) {
+        console.error("Update project error:", error);
+        setMessage("An error occured while saving your row count");
+      }
+    });
+  }
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent>
         <DialogClose />
-        <DialogTitle>Rowcounter</DialogTitle>
-        <DialogDescription>Set your current row count.</DialogDescription>
-        <form key={formValue} className="mt-2" action={formAction}>
-          <div className="grid gap-3">
-            <label htmlFor="count" className="ml-2">
-              Count
-            </label>
-            <div className="flex items-center gap-4">
-              <NumberSpinner
-                className="max-w-34"
-                min={1}
-                max={99}
-                defaultValue={formValue}
-                id="count"
-                name="count"
-                accessibleName="count"
-                required
-              />
-
-              <Button
-                type="button"
-                variant="ghost"
-                className="w-fit p-0 text-base text-muted-foreground hover:bg-transparent hover:text-foreground/80 focus-visible:text-foreground focus-visible:ring-transparent focus-visible:outline-none active:text-foreground dark:hover:bg-transparent"
-                onClick={() => {
-                  setFormValue(1);
-                }}
-              >
-                <IterationCw className="size-4" />
-                set to 1
-              </Button>
-            </div>
-          </div>
-
-          {state.errors && <FormError>{state.message}</FormError>}
-          <DialogFooter className="mt-6">
-            <Button
-              className="ml-auto w-[4.75rem]"
-              onClick={() => {
-                setFormValue(count);
-                setOpen(false);
-              }}
-              variant="secondary"
-              disabled={pending}
+        <DialogTitle>Row Count</DialogTitle>
+        <DialogDescription>
+          Set your current row count or reset your counter to start from the
+          beginning.
+        </DialogDescription>
+        <div className="grid gap-3">
+          <div className="flex items-center gap-4">
+            <SpinButton
+              id="rowCount"
+              min={1}
+              max={99}
+              defaultValue={count}
+              value={value}
+              onValueChange={setValue}
             >
-              cancel
+              <SpinButtonGroup>
+                <SpinButtonLabel className="sr-only">
+                  currentCount
+                </SpinButtonLabel>
+                <SpinButtonDecrement title="decrement current count by 1" />
+                <SpinButtonField />
+                <SpinButtonIncrement title="increment current count by 1" />
+              </SpinButtonGroup>
+              <SpinButtonError />
+            </SpinButton>
+            <Button
+              onClick={() => setValue("1")}
+              type="button"
+              variant="ghost"
+              className="w-fit p-0 text-base text-muted-foreground hover:bg-transparent hover:text-foreground/80 focus-visible:text-foreground focus-visible:ring-transparent focus-visible:outline-none active:text-foreground dark:hover:bg-transparent"
+            >
+              <IterationCw className="size-4" />
+              set to 1
             </Button>
-            <Button disabled={pending} type="submit" className="w-[4.75rem]">
-              save
-            </Button>
-          </DialogFooter>
-        </form>
+          </div>
+        </div>
+
+        {message && <FormError>{message}</FormError>}
+        <DialogFooter className="mt-6">
+          <Button
+            className="ml-auto w-[4.75rem]"
+            onClick={() => {
+              setOpen(false);
+            }}
+            variant="secondary"
+            disabled={pending}
+          >
+            cancel
+          </Button>
+          <Button
+            disabled={pending}
+            onClick={submitProjectCount}
+            className="w-[4.75rem]"
+          >
+            {pending ? <Loader className="animate-spin" /> : "save"}
+          </Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
