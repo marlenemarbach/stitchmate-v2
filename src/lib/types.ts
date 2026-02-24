5d6bdc82791fd2cbd9b00049b9e59e4715c3abcd
Marlene <marlene.goedecke@ui.city>
2026-02-24 16:08:39 +0100

    add User

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,5 @@
 import { InferSelectModel } from "drizzle-orm";
-import { projects, subCounters } from "@/db/schema";
+import { projects, subCounters, users } from "@/db/schema";
 
 export type Project = InferSelectModel<typeof projects>;
 export type ProjectStatus = Project["status"];
@@ -12,3 +12,5 @@ export type CountDirection = 1 | -1;
 export type ProjectWithSubCounter = Project & {
   subCounter: SubCounter;
 };
+
+export type User = InferSelectModel<typeof users>;
