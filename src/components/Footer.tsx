f1d38d5437d08069e5414929297a32f86b83e43b
Marlene <marlene.goedecke@ui.city>
2026-03-04 12:08:19 +0100

    modify footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,18 @@
-"use server";
-
 import { cn } from "@/lib/utils";
 
 export async function Footer() {
   return (
-    <footer className={cn("flex justify-center gap-6 px-5 pt-5 pb-6 text-xs")}>
-      <a
-        className="ease opacity-40 transition-opacity duration-150 outline-none hover:opacity-60 focus-visible:opacity-70"
-        href="https://www.instagram.com/crafty_stitchess/"
-        target="_blank"
-      >
-        created by @craftystitchess
-      </a>
+    <footer className={cn("mx-auto mt-auto pt-5 pb-6 text-xs")}>
+      <p className="flex gap-1 text-muted-foreground/70">
+        created by
+        <a
+          className="ease transition-colors duration-150 outline-none hover:text-muted-foreground focus-visible:text-muted-foreground"
+          href="https://www.instagram.com/crafty_stitchess/"
+          target="_blank"
+        >
+          @craftystitchess
+        </a>
+      </p>
     </footer>
   );
 }
