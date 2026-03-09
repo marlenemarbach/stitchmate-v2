40afa10332206e89ae29cc0dc9871fb507caba32
Marlene <marlene.goedecke@ui.city>
2026-03-09 09:42:10 +0100

    modify footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,13 +4,12 @@ export async function Footer() {
   return (
     <footer className={cn("mx-auto mt-auto pt-5 pb-6 text-xs")}>
       <p className="flex gap-1 text-muted-foreground/70">
-        created by
+        by
         <a
           className="ease transition-colors duration-150 outline-none hover:text-muted-foreground focus-visible:text-muted-foreground"
-          href="https://www.instagram.com/crafty_stitchess/"
-          target="_blank"
+          href="mailto:m.marbach.me@gmail.com"
         >
-          @craftystitchess
+          Marlene Marbach
         </a>
       </p>
     </footer>
