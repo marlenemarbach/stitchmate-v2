424927af956d5c9f5183a2b3767c85e849264909
Marlene <m.marbach.me@gmail.com>
2026-04-27 17:49:34 +0200

    remove datalist component for simplicity and refactor responsive styles

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,25 +1,51 @@
-import { use } from "react";
+"use client";
+
+import { use, useRef } from "react";
 import { Project } from "@/lib/types";
 import { CreateProjectDialog } from "./CreateProjectDialog";
 import { ProjectListItem } from "./ProjectListItem";
 import { ProjectOrderButtons } from "./ProjectOrderButtons";
-import { DataList, DataListContent, DataListHeader } from "./ui/DataList";
+import { Pagination } from "./ui/Pagination";
 
 export function ProjectList({ projects }: { projects: Promise<Project[]> }) {
   const allProjects = use(projects);
 
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  function handleUpAndDownArrowKeys(e: React.KeyboardEvent<HTMLDivElement>) {
+    if (e.key !== "ArrowDown" && e.key !== "ArrowUp") return;
+
+    if (!contentRef.current) return;
+
+    const listItems = Array.from(
+      contentRef.current.querySelectorAll<HTMLElement>("[data-list-item]"),
+    );
+    const index = listItems.indexOf(document.activeElement as HTMLElement);
+
+    if (e.key === "ArrowDown") {
+      listItems[index + 1]?.focus();
+    } else {
+      listItems[index - 1]?.focus();
+    }
+  }
+
   return (
     <>
-      <div className="fixed top-14 flex w-full items-center justify-between bg-background px-4 py-3">
+      <div className="fixed top-14 flex w-screen items-center justify-between bg-background px-4 py-3">
         <h1 className="text-lg font-medium">My Projects</h1>
         <CreateProjectDialog />
       </div>
-      <DataList>
+
+      {/* -------------- main grid -------------- */}
+      <div className="grid w-full max-w-2xl grid-cols-[1fr_auto] sm:grid-cols-[1fr_6rem_8.3rem_3rem]">
+        {/* -------------- header -------------- */}
         {allProjects.length > 0 && (
-          <DataListHeader className="fixed top-28 w-full max-w-3xl">
+          <div className="fixed top-28 col-span-4 grid w-full max-w-2xl grid-cols-[auto_auto_1fr] items-start bg-background mask-b-from-80% mask-b-to-90% py-2 sm:grid-cols-[1fr_6rem_8.3rem_3rem]">
             <ProjectOrderButtons />
-          </DataListHeader>
+          </div>
         )}
+
+        {/* -------------- content -------------- */}
         {!allProjects.length ? (
           <div className="flex max-w-xs flex-1 flex-col items-center justify-center gap-6 self-center justify-self-center">
             <div className="flex items-end gap-3">
@@ -27,13 +53,18 @@ export function ProjectList({ projects }: { projects: Promise<Project[]> }) {
             </div>
           </div>
         ) : (
-          <DataListContent className="mt-40">
+          <div
+            className="col-span-2 mt-40 grid w-full grid-cols-subgrid grid-rows-2 overflow-y-auto sm:col-span-4 sm:grid-rows-1"
+            ref={contentRef}
+            onKeyDown={(e) => handleUpAndDownArrowKeys(e)}
+          >
             {allProjects.map((project) => {
               return <ProjectListItem key={project.id} project={project} />;
             })}
-          </DataListContent>
+          </div>
         )}
-      </DataList>
+      </div>
+      <Pagination></Pagination>
     </>
   );
 }
--- src/components/ProjectListItem.tsx
+++ src/components/ProjectListItem.tsx
@@ -1,28 +1,123 @@
 "use client";
 
+import { useState, useTransition } from "react";
 import Link from "next/link";
-import { type Project } from "@/lib/types";
+import { PencilIcon, TrashIcon } from "@heroicons/react/16/solid";
+import { toast } from "sonner";
+import { updateProject } from "@/actions/projects";
+import { PROJECT_STATUSES } from "@/lib/constants";
+import { type Project, ProjectStatus } from "@/lib/types";
 import { calculateTimeSince } from "@/lib/utils";
-import { ProjectListMenu } from "./ProjectListMenu";
-import { DataListItem } from "./ui/DataList";
+import { DeleteProjectDialog } from "./DeleteProjectDialog";
+import { EditProjectDialog } from "./EditProjectDialog";
+import { ProjectDropdownMenu } from "./ProjectDropdownMenu";
+import { ProjectStatusMenu } from "./ProjectStatusMenu";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuLabel,
+  ContextMenuRadioGroup,
+  ContextMenuRadioItem,
+  ContextMenuSeparator,
+  ContextMenuTrigger,
+} from "./ui/ContextMenu";
 import { StatusBadge } from "./ui/StatusBadge";
 
 export function ProjectListItem({ project }: { project: Project }) {
+  const [showEditDialog, setShowEditDialog] = useState(false);
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+
+  console.log(showEditDialog);
+
+  const [isPending, startTransition] = useTransition();
+
+  function updateProjectStatus(newStatus: ProjectStatus) {
+    if (newStatus === project.status) return;
+
+    startTransition(async () => {
+      const data = { status: newStatus };
+      try {
+        const result = await updateProject(data, project.id);
+        if (!result.success) {
+          toast.error(`Something went wrong while updating ${project.name}`);
+        }
+        return;
+      } catch (e) {
+        console.error("update project status error:", e);
+        toast.error(`Something went wrong while updating ${project.name}`);
+      }
+    });
+  }
+
   return (
-    <DataListItem className="data-[state=active]:bg-foreground/5">
-      <Link
-        href={`/projects/${project.id}`}
-        className="col-span-5 grid grid-cols-subgrid gap-1 sm:gap-2"
-      >
-        <h3 className="col-span-5 font-medium sm:col-span-3">{project.name}</h3>
-        <div className="col-span-5 flex gap-2 sm:col-span-2 sm:grid sm:grid-cols-subgrid">
-          <StatusBadge status={project.status} />
-          <p className="text-sm text-muted-foreground">
-            {calculateTimeSince(project.updatedAt)}
-          </p>
-        </div>
-      </Link>
-      <ProjectListMenu project={project} />
-    </DataListItem>
+    <>
+      <ContextMenu>
+        <ContextMenuTrigger asChild>
+          <Link
+            className="col-span-2 grid min-h-11 cursor-default grid-cols-subgrid items-center gap-x-2 gap-y-1 border-b border-border/50 px-4 py-3 focus-visible:bg-foreground/3 focus-visible:outline-none sm:col-span-4 sm:rounded-full sm:border-none sm:py-2 sm:focus-within:bg-foreground/3 sm:hover:bg-foreground/3 sm:has-[[data-state=open]]:bg-foreground/3"
+            href={`/projects/${project.id}`}
+            data-list-item
+          >
+            <h3 className="font-medium">{project.name}</h3>
+
+            <div className="align-center row-start-2 grid grid-cols-[20px_auto] sm:col-span-2 sm:row-start-auto sm:grid-cols-subgrid">
+              <ProjectStatusMenu
+                status={project.status}
+                onStatusChange={updateProjectStatus}
+                isPending={isPending}
+              />
+              <p className="mt-0.5 text-sm text-muted-foreground sm:mt-0">
+                {calculateTimeSince(project.updatedAt)}
+              </p>
+            </div>
+
+            <ProjectDropdownMenu
+              status={project.status}
+              onStatusChange={updateProjectStatus}
+              setShowDeleteDialog={setShowDeleteDialog}
+              setShowEditDialog={setShowEditDialog}
+            />
+          </Link>
+        </ContextMenuTrigger>
+
+        <ContextMenuContent loop onClick={(e) => e.preventDefault()}>
+          <ContextMenuItem onSelect={() => setShowEditDialog(true)}>
+            <PencilIcon />
+            <span className="col-start-2">Edit...</span>
+          </ContextMenuItem>
+          <ContextMenuSeparator />
+          <ContextMenuRadioGroup
+            value={project.status}
+            onValueChange={(value) =>
+              updateProjectStatus(value as ProjectStatus)
+            }
+          >
+            <ContextMenuLabel>Status</ContextMenuLabel>
+            {PROJECT_STATUSES.map((status) => (
+              <ContextMenuRadioItem key={status} value={status}>
+                <StatusBadge className="size-4" status={status} />
+                {status}
+              </ContextMenuRadioItem>
+            ))}
+          </ContextMenuRadioGroup>
+          <ContextMenuSeparator />
+          <ContextMenuItem onSelect={() => setShowDeleteDialog(true)}>
+            <TrashIcon /> Delete
+          </ContextMenuItem>
+        </ContextMenuContent>
+      </ContextMenu>
+
+      <EditProjectDialog
+        project={project}
+        open={showEditDialog}
+        setOpen={setShowEditDialog}
+      />
+      <DeleteProjectDialog
+        project={project}
+        open={showDeleteDialog}
+        setOpen={setShowDeleteDialog}
+      />
+    </>
   );
 }
