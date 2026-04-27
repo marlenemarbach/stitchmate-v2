fe0440a924e5cb687360ed3319bec94375c49a17
Marlene <m.marbach.me@gmail.com>
2026-04-27 17:47:21 +0200

    update  radio group styles

--- src/components/ui/RadioGroup.tsx
+++ src/components/ui/RadioGroup.tsx
@@ -1,15 +1,18 @@
 "use client";
 
+// import { startTransition } from "react";
 import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
 import { cn } from "@/lib/utils";
 
 export function RadioGroup({
   className,
+  onValueChange,
   ...props
 }: React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root>) {
   return (
     <RadioGroupPrimitive.Root
       className={cn("grid gap-3", className)}
+      onValueChange={onValueChange}
       {...props}
     />
   );
@@ -23,7 +26,7 @@ export function RadioItem({
   return (
     <RadioGroupPrimitive.Item
       className={cn(
-        "flex h-8 items-center gap-2 rounded-full p-2 px-3 text-sm opacity-70 shadow-xs transition-all duration-150 ease-out outline-none hover:bg-foreground/5 hover:opacity-100 focus-visible:ring-[1.5px] focus-visible:ring-ring disabled:opacity-50 data-[state=checked]:bg-foreground/5 data-[state=checked]:opacity-100 [&_svg:not([class*='size-'])]:size-4",
+        "flex h-8 items-center gap-2 rounded-full p-2 px-3 text-sm opacity-70 shadow-xs transition-all duration-150 ease-out outline-none focus-visible:ring-[1.5px] focus-visible:ring-ring disabled:opacity-50 data-[state=checked]:bg-foreground/5 data-[state=checked]:opacity-100 sm:hover:bg-foreground/5 sm:hover:opacity-100 [&_svg:not([class*='size-'])]:size-4",
         className,
       )}
       {...props}
