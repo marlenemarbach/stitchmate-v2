563534b7e00840d9ca27190f0abf5a6d160f67a3
Marlene <m.marbach.me@gmail.com>
2026-04-27 17:44:36 +0200

    update button styles

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,7 +3,7 @@ import { type VariantProps, cva } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 export const buttonVariants = cva(
-  "flex focus-visible:ring-[1.5px] focus-visible:ring-ring items-center disabled:opacity-50 justify-center gap-2 whitespace-nowrap rounded-full transition-[color,background-color] disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none  ease-[ease] duration-150 ",
+  "flex focus-visible:inset-ring-[1.5px] focus-visible:ring-ring items-center disabled:opacity-50 justify-center gap-2 whitespace-nowrap rounded-full transition-[color,background-color] disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none  ease-[ease] duration-150 ",
   {
     variants: {
       variant: {
@@ -14,11 +14,11 @@ export const buttonVariants = cva(
         accent: "font-medium bg-accent/90 hover:bg-accent",
         ghost: "hover:bg-foreground/5 rounded-full",
         destructive:
-          "text-red-400 font-medium border border-border bg-foreground/10 hover:bg-foreground/13 shadow-xs",
+          "text-red-400 font-medium border border-border bg-foreground/5 hover:bg-foreground/8 shadow-xs",
       },
       size: {
         default: "h-9 px-4 py-2 text-sm",
-        small: "h-8 py-1 px-3 gap-1 sm:text-sm",
+        small: "h-8 py-1 px-3 gap-1 text-sm",
         large: "h-11 text-base",
         icon: "w-8 h-8 p-2",
       },
