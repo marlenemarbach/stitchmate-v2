a7e64b704b723cd9d47d3557f275dc973a08ed9a
Marlene <m.marbach.me@gmail.com>
2026-04-27 17:58:24 +0200

    update suspense visual

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import { SuspenseLogo } from "@/components/svg/SuspenseLogo";
 export default function Loading() {
   return (
     <div className="flex flex-1 items-center justify-center">
-      <SuspenseLogo className="mb-10 opacity-50" />
+      <SuspenseLogo className="text-planned opacity-25" />
     </div>
   );
 }
